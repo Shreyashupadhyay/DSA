7cf1d23c6f79e27eb13eedb07d58250c159c00da
shreyash <shreyashamyt@gmail.com>
2022-03-28 14:42:13 +0530

    new

---
+++ binary_tree_deletion.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<malloc.h>
+ 
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+ 
+struct node* createNode(int data){
+    struct node *n; // creating a node pointer
+    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data; // Setting the data
+    n->left = NULL; // Setting the left and right children to NULL
+    n->right = NULL; // Setting the left and right children to NULL
+    return n; // Finally returning the created node
+}
+
+struct node *inOrderPredecessor(struct node* root){
+    root = root->left;
+    while (root->right!=NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct node *deleteNode(struct node *root, int value){
+
+    struct node* iPre;
+    if (root == NULL){
+        return NULL;
+    }
+    if (root->left==NULL&&root->right==NULL){
+        free(root);
+        return NULL;
+    }
+    //searching for the node to be deleted
+    if (value < root->data){
+        root-> left = deleteNode(root->left,value);
+    }
+    else if (value > root->data){
+        root-> right = deleteNode(root->right,value);
+    }
+    //deletion strategy when the node is found
+    else{
+        iPre = inOrderPredecessor(root);
+        root->data = iPre->data;
+        root->left = deleteNode(root->left, iPre->data);
+    }
+    return root;
+}
